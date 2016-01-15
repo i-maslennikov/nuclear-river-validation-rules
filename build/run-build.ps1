ec13debe872e0e6d0ab9fb570b2097a9e98ed5e1
m.pashuk <m.pashuk@2gis.ru>
2016-01-15 14:37:35 +0600

    buildtools-update

--- build/run-build.ps1
+++ build/run-build.ps1
@@ -1,14 +1,14 @@
 ﻿param([string[]]$TaskList = @(), [hashtable]$Properties = @{})
 
 if ($TaskList.Count -eq 0){
-	$TaskList = @('Build-Packages', 'Deploy-Packages')
+	$TaskList = @('Build-Packages')
 }
 
 if ($Properties.Count -eq 0){
  	$Properties.EnvironmentName = 'Test.20'
 	$Properties.EntryPoints = @(
-		#'Web.OData'
-		#'Replication.EntryPoint'
+		'Web.OData'
+		'Replication.EntryPoint'
 	)
 	#$Properties.UseCaseRoute = 'ERM'
 	#$Properties.UpdateSchemas = 'CustomerIntelligence'
@@ -25,18 +25,18 @@ $Properties.BuildFile = Join-Path $PSScriptRoot 'default.ps1'
 
 # Restore-Packages
 & {
-	$NugetPath = Join-Path $Properties.SolutionDir '.nuget\NuGet.exe'
+	$NugetPath = Join-Path $Properties.SolutionDir '.nuget\NuGet_v3.3.0.exe'
 	if (!(Test-Path $NugetPath)){
 		$webClient = New-Object System.Net.WebClient
 		$webClient.UseDefaultCredentials = $true
 		$webClient.Proxy.Credentials = $webClient.Credentials
-		$webClient.DownloadFile('https://www.nuget.org/nuget.exe', $NugetPath)
+		$webClient.DownloadFile('https://dist.nuget.org/win-x86-commandline/v3.3.0/nuget.exe', $NugetPath)
 	}
 	$solution = Get-ChildItem $Properties.SolutionDir -Filter '*.sln'
 	& $NugetPath @('restore', $solution.FullName, '-NonInteractive', '-Verbosity', 'quiet')
 }
 
-Import-Module "$($Properties.SolutionDir)\packages\2GIS.NuClear.BuildTools.0.2.24\tools\buildtools.psm1" -DisableNameChecking -Force
+Import-Module "$($Properties.SolutionDir)\packages\2GIS.NuClear.BuildTools.0.2.29\tools\buildtools.psm1" -DisableNameChecking -Force
 Import-Module "$PSScriptRoot\metadata.psm1" -DisableNameChecking -Force
 
 Add-Metadata (Parse-EnvironmentMetadata $Properties)
