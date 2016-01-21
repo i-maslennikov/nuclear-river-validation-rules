b52ba1cb08aa578716fd5772b07f4ccd6d10fb85
a.rechkalov <a.rechkalov@2gis.ru>
2016-01-21 17:39:26 +0600

    Уточнил название сущности

--- ValidationRules/ValidationRules.Domain/Model/Aggregates/AdvertisementAmountRestriction.cs
+++ ValidationRules/ValidationRules.Domain/Model/Aggregates/AdvertisementAmountRestriction.cs
@@ -3,7 +3,7 @@ namespace NuClear.ValidationRules.Domain.Model.Aggregates
     /// <summary>
     /// Связь прайс-листа с номеклатурной позицией, импортируется из ERM
     /// </summary>
-    public class PricePosition
+    public class AdvertisementAmountRestriction
     {
         public long PriceId { get; set; }
         public long PositionId { get; set; }
