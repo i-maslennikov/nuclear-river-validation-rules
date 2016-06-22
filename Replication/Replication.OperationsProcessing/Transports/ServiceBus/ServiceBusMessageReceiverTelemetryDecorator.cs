1766c4d2e7f8ce2dbe6ce79046cdadbb5c771b30
a.rechkalov <a.rechkalov@2gis.ru>
2016-06-22 13:50:59 +0600

    Удалил логирование ErmReceivedUseCaseCountIdentity

--- Replication/Replication.OperationsProcessing/Transports/ServiceBus/ServiceBusMessageReceiverTelemetryDecorator.cs
+++ Replication/Replication.OperationsProcessing/Transports/ServiceBus/ServiceBusMessageReceiverTelemetryDecorator.cs
@@ -12,12 +12,10 @@ namespace NuClear.Replication.OperationsProcessing.Transports.ServiceBus
     public sealed class ServiceBusMessageReceiverTelemetryDecorator : IMessageReceiver
     {
         private readonly IMessageReceiver _receiver;
-        private readonly ITelemetryPublisher _telemetryPublisher;
 
-        public ServiceBusMessageReceiverTelemetryDecorator(ServiceBusMessageReceiver receiver, ITelemetryPublisher telemetryPublisher)
+        public ServiceBusMessageReceiverTelemetryDecorator(ServiceBusMessageReceiver receiver)
         {
             _receiver = receiver;
-            _telemetryPublisher = telemetryPublisher;
         }
 
         public IReadOnlyList<IMessage> Peek()
@@ -25,7 +23,6 @@ namespace NuClear.Replication.OperationsProcessing.Transports.ServiceBus
             using (Probe.Create("Peek messages from ServiceBus"))
             {
                 var messages = _receiver.Peek();
-                _telemetryPublisher.Publish<ErmReceivedUseCaseCountIdentity>(messages.Count);
                 return messages;
             }
         }
