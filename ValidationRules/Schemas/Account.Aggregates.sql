0b20f91c2056edd3429c18b6526de036b3f36cc4
a.rechkalov <a.rechkalov@2gis.ru>
2016-07-01 16:24:44 +0600

    Добавил наборки схемы для правила AccountExistsOrderValidationRule

---
+++ ValidationRules/Schemas/Account.Aggregates.sql
@@ -0,0 +1,12 @@
+﻿if not exists (select * from sys.schemas where name = 'PriceAggregate') exec('create schema PriceAggregate')
+
+if object_id('AccountAggregate.Order') is not null drop table AccountAggregate.[Order]
+go
+
+create table PriceContext.[Order](
+    Id bigint not null,
+    Number nvarchar(max) not null,
+    HasAccount bit not null,
+    constraint PK_Order primary key (Id)
+)
+go
---
+++ ValidationRules/Schemas/Account.Facts.sql
@@ -0,0 +1,20 @@
+﻿if not exists (select * from sys.schemas where name = 'AccountContext') exec('create schema AccountContext')
+go
+
+if object_id('AccountContext.Order') is not null drop table PriceContext.[Order]
+if object_id('AccountContext.Account') is not null drop table PriceContext.Account
+go
+
+create table PriceContext.[Order](
+    Id bigint not null,
+    AccountId bigint null,
+    Number nvarchar(max) not null,
+    constraint PK_Order primary key (Id)
+)
+go
+
+create table PriceContext.Account(
+    Id bigint not null,
+    constraint PK_Account primary key (Id)
+)
+go
