1d189e8dabc232d457b3a1d93647a75894bd3232
a.rechkalov <a.rechkalov@2gis.ru>
2016-07-13 16:21:14 +0600

    Реализовал сериализацию сообщения проверки

--- ValidationRules/ValidationRules.Replication.Host/ResultDelivery/Serializers/AccountBalanceShouldBePositiveMessageSerializer.cs
+++ ValidationRules/ValidationRules.Replication.Host/ResultDelivery/Serializers/AccountBalanceShouldBePositiveMessageSerializer.cs
@@ -1,13 +1,27 @@
 ﻿using System.Xml.Linq;
 
+using NuClear.ValidationRules.Replication.AccountRules.Validation;
+
 namespace NuClear.ValidationRules.Replication.Host.ResultDelivery.Serializers
 {
     public sealed class AccountBalanceShouldBePositiveMessageSerializer : IMessageSerializer
     {
-        public int MessageType { get; }
-        public LocalizedMessage Serialize(XDocument messageParams)
+        public int MessageType
+            => AccountBalanceShouldBePositiveActor.MessageTypeId;
+
+        private readonly LinkFactory _linkFactory = new LinkFactory();
+
+        public LocalizedMessage Serialize(XDocument document)
         {
-            throw new System.NotImplementedException();
+            var orderId = (long)document.Root.Element("order").Attribute("id");
+            var orderNumber = (string)document.Root.Element("order").Attribute("number");
+            var available = (decimal)document.Root.Element("message").Attribute("available");
+            var planned = (decimal)document.Root.Element("message").Attribute("planned");
+            var required = (decimal)document.Root.Element("message").Attribute("required");
+
+            return new LocalizedMessage(Result.Error,
+                                        $"Заказ <{_linkFactory.CreateLink("Order", orderId)}|{orderNumber}>",
+                                        $"Для оформления заказа недостаточно средств. Необходимо: {planned}. Имеется: {available}. Необходим лимит: {required}");
         }
     }
 }
---
+++ ValidationRules/ValidationRules.Replication.Host/ResultDelivery/Serializers/LinkFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuClear.ValidationRules.Replication.Host.ResultDelivery.Serializers
+{
+    public sealed class LinkFactory
+    {
+        public string CreateLink(string entityName, long entityId)
+            => $"https://web-app20.test.erm.2gis.ru/CreateOrUpdate/{entityName}/{entityId}";
+    }
+}
