fb47face54961dffa8438610974b950faeea2333
a.rechkalov <a.rechkalov@2gis.ru>
2016-07-19 14:55:44 +0600

    Подправил формат сообщения AssociatedPositionWithoutPrincipalActor

--- ValidationRules/ValidationRules.Replication/PriceRules/Validation/AssociatedPositionWithoutPrincipalActor.cs
+++ ValidationRules/ValidationRules.Replication/PriceRules/Validation/AssociatedPositionWithoutPrincipalActor.cs
@@ -11,9 +11,18 @@ using Version = NuClear.ValidationRules.Storage.Model.Messages.Version;
 
 namespace NuClear.ValidationRules.Replication.PriceRules.Validation
 {
+    /// <summary>
+    /// Для заказа, в котором есть сопутствующая позиция и нет основной должна выводиться ошибка.
+    /// "{0} является сопутствующей, основная позиция не найдена"
+    /// 
+    /// Source: ADP/AssociatedPositionWithoutPrincipalTemplate
+    /// 
+    /// Q1: Может ли элемент пакета быть удовлетворён другим элементом того-же самого пакета?
+    /// 
+    /// Q2: Может ли пакет быть удовлетворён своим элементом или наоборот?
+    /// </summary>
     public sealed class AssociatedPositionWithoutPrincipalActor : IActor
     {
-        // AssociatedPositionWithoutPrincipalTemplate - {0} является сопутствующей, основная позиция не найдена
         private const int MessageTypeId = 9;
 
         private static readonly int RuleResult = new ResultBuilder().WhenSingle(Result.Error)
@@ -60,20 +69,20 @@ namespace NuClear.ValidationRules.Replication.PriceRules.Validation
 
             var notSatisfiedPositions =
                 (from position in associatedPositions
-                from satisfied in satisfiedPositions.Where(x => x.CauseOrderPositionId == position.position.CauseOrderPositionId &&
-                                                                x.CauseItemPositionId == position.position.CauseItemPositionId).DefaultIfEmpty()
-                where satisfied == null
-                select new
-                {
-                    position.FirmId,
-                    position.position.OrderId,
-                    position.position.CauseOrderPositionId,
-                    position.position.CausePackagePositionId,
-                    position.position.CauseItemPositionId,
+                 from satisfied in satisfiedPositions.Where(x => x.CauseOrderPositionId == position.position.CauseOrderPositionId &&
+                                                                 x.CauseItemPositionId == position.position.CauseItemPositionId).DefaultIfEmpty()
+                 where satisfied == null
+                 select new
+                 {
+                     position.FirmId,
+                     position.position.OrderId,
+                     position.position.CauseOrderPositionId,
+                     position.position.CausePackagePositionId,
+                     position.position.CauseItemPositionId,
 
-                    position.Start,
-                    position.OrganizationUnitId,
-                }).Distinct(); // схлопнем позиции по всем нарушенным правилам
+                     position.Start,
+                     position.OrganizationUnitId,
+                 }).Distinct(); // схлопнем позиции по всем нарушенным правилам
 
             var messages = from conflict in notSatisfiedPositions
                            join period in query.For<Period>() on new { conflict.Start, conflict.OrganizationUnitId } equals new { period.Start, period.OrganizationUnitId }
@@ -83,15 +92,19 @@ namespace NuClear.ValidationRules.Replication.PriceRules.Validation
                                MessageType = MessageTypeId,
 
                                MessageParams =
-                                    new XDocument(new XElement("element",
-                                                               new XAttribute("firm", conflict.FirmId),
+                                    new XDocument(new XElement("root",
+                                                               new XElement("firm",
+                                                                            new XAttribute("id", conflict.FirmId)),
                                                                new XElement("position",
                                                                             new XAttribute("orderId", conflict.OrderId),
                                                                             new XAttribute("orderNumber", query.For<Order>().Single(x => x.Id == conflict.OrderId).Number),
                                                                             new XAttribute("orderPositionId", conflict.CauseOrderPositionId),
                                                                             new XAttribute("orderPositionName", query.For<Position>().Single(x => x.Id == conflict.CausePackagePositionId).Name),
                                                                             new XAttribute("positionId", conflict.CauseItemPositionId),
-                                                                            new XAttribute("positionName", query.For<Position>().Single(x => x.Id == conflict.CauseItemPositionId).Name)))),
+                                                                            new XAttribute("positionName", query.For<Position>().Single(x => x.Id == conflict.CauseItemPositionId).Name)),
+                                                               new XElement("order",
+                                                                            new XAttribute("id", conflict.OrderId),
+                                                                            new XAttribute("number", query.For<Order>().Single(x => x.Id == conflict.OrderId).Number)))),
 
                                PeriodStart = period.Start,
                                PeriodEnd = period.End,
