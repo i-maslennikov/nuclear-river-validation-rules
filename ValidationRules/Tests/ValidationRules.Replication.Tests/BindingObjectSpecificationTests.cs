531152f2fe8313e4cec2f3e4ee01cd605b5b15a2
a.rechkalov <a.rechkalov@2gis.ru>
2016-07-20 18:17:51 +0600

    Восстановил тест

--- ValidationRules/Tests/ValidationRules.Replication.Tests/BindingObjectSpecificationTests.cs
+++ ValidationRules/Tests/ValidationRules.Replication.Tests/BindingObjectSpecificationTests.cs
@@ -1,138 +1,141 @@
-﻿//using System.Collections.Generic;
-//using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
-//using NuClear.ValidationRules.Replication.PriceRules.Validation.Dto;
-//using NuClear.ValidationRules.Replication.Specifications;
-//using NuClear.ValidationRules.Storage.Model.PriceRules.Aggregates;
+using NuClear.ValidationRules.Replication.PriceRules.Validation.Dto;
+using NuClear.ValidationRules.Replication.Specifications;
+using NuClear.ValidationRules.Storage.Model.PriceRules.Aggregates;
 
-//using NUnit.Framework;
+using NUnit.Framework;
 
-//namespace NuClear.ValidationRules.Replication.Tests
-//{
-//    [TestFixture]
-//    public sealed class BindingObjectSpecificationTests
-//    {
-//        [TestCaseSource(nameof(Examples))]
-//        public void Ok(Parameter left, Parameter right, bool matches)
-//        {
-//            var func = Specs.Find.Aggs.BindingObjectMatch().Compile();
-//            var test1 = new PrincipalAssociatedPostionPair
-//                {
-//                    OrderAssociatedPosition = CreateOrderAssociatedPosition(left),
-//                    OrderPrincipalPosition = CreateOrderPrincipalPosition(right),
-//                };
+namespace NuClear.ValidationRules.Replication.Tests
+{
+    [TestFixture]
+    public sealed class BindingObjectSpecificationTests
+    {
+        [TestCaseSource(nameof(Examples))]
+        public void Ok(Parameter left, Parameter right, bool matches)
+        {
+            Assert.That(InvokeSpecification(left, right), Is.EqualTo(matches), "Expression is not correct");
+            Assert.That(InvokeSpecification(right, left), Is.EqualTo(matches), "Expression is not commutative");
+        }
 
-//            var test2 = new PrincipalAssociatedPostionPair
-//                {
-//                    OrderAssociatedPosition = CreateOrderAssociatedPosition(right),
-//                    OrderPrincipalPosition = CreateOrderPrincipalPosition(left),
-//                };
+        private static bool InvokeSpecification(Parameter left, Parameter right)
+        {
+            var principal = CreateOrderPrincipalPosition(left);
+            var associated = CreateOrderAssociatedPosition(right);
 
-//            Assert.That(func.Invoke(test1), Is.EqualTo(matches), "Expression is not correct");
-//            Assert.That(func.Invoke(test1), Is.EqualTo(func.Invoke(test2)), "Expression is not commutative");
-//        }
+            return Specs.Join.Aggs.WithMatchedBindingObject(new[] { principal }.AsQueryable()).Compile().Invoke(associated).Any();
+        }
 
-//        private static OrderPosition CreateOrderPrincipalPosition(Parameter parameter)
-//            => new OrderPosition
-//            {
-//                HasNoBinding = parameter.HasNoBinding,
-//                Category1Id = parameter.Category1Id,
-//                Category3Id = parameter.Category3Id,
-//                FirmAddressId = parameter.FirmAddressId,
-//            };
+        private static Dto<OrderPosition> CreateOrderPrincipalPosition(Parameter parameter)
+            => new Dto<OrderPosition>
+                {
+                    Position = new OrderPosition
+                        {
+                            HasNoBinding = parameter.HasNoBinding,
+                            Category1Id = parameter.Category1Id,
+                            Category3Id = parameter.Category3Id,
+                            FirmAddressId = parameter.FirmAddressId,
+                        }
+                };
 
-//        private static OrderAssociatedPosition CreateOrderAssociatedPosition(Parameter parameter)
-//            => new OrderAssociatedPosition
-//                {
-//                    HasNoBinding = parameter.HasNoBinding,
-//                    Category1Id = parameter.Category1Id,
-//                    Category3Id = parameter.Category3Id,
-//                    FirmAddressId = parameter.FirmAddressId,
-//                };
+        private static Dto<OrderAssociatedPosition> CreateOrderAssociatedPosition(Parameter parameter)
+            => new Dto<OrderAssociatedPosition>
+                {
+                    Position = new OrderAssociatedPosition
+                        {
+                            CauseOrderPositionId = 1,
 
-//        private IEnumerable<TestCaseData> Examples()
-//        {
-//            var noBinding = new Parameter { HasNoBinding = true };
-//            var category3 = new Parameter { Category3Id = 3, Category1Id = 1 };
-//            var category1 = new Parameter { Category1Id = 1 }; // Не бывает по бизнесу
-//            var addressCategory3 = new Parameter { Category3Id = 3, Category1Id = 1, FirmAddressId = 1 };
-//            var addressCategory1 = new Parameter { Category1Id = 1, FirmAddressId = 1 };
-//            var address = new Parameter { FirmAddressId = 1 };
+                            HasNoBinding = parameter.HasNoBinding,
+                            Category1Id = parameter.Category1Id,
+                            Category3Id = parameter.Category3Id,
+                            FirmAddressId = parameter.FirmAddressId,
+                        }
+                };
 
-//            var anotherCategory3 = new Parameter { Category3Id = 33, Category1Id = 11 };
-//            var anotherCategory1 = new Parameter { Category1Id = 11 }; // Не бывает по бизнесу
-//            var anotherAddressCategory3 = new Parameter { Category3Id = 33, Category1Id = 11, FirmAddressId = 11 };
-//            var anotherAddressCategory1 = new Parameter { Category1Id = 11, FirmAddressId = 11 };
-//            var anotherAddress = new Parameter { FirmAddressId = 11 };
+        private IEnumerable<TestCaseData> Examples()
+        {
+            var noBinding = new Parameter { HasNoBinding = true };
+            var category3 = new Parameter { Category3Id = 3, Category1Id = 1 };
+            var category1 = new Parameter { Category1Id = 1 }; // Не бывает по бизнесу
+            var addressCategory3 = new Parameter { Category3Id = 3, Category1Id = 1, FirmAddressId = 1 };
+            var addressCategory1 = new Parameter { Category1Id = 1, FirmAddressId = 1 };
+            var address = new Parameter { FirmAddressId = 1 };
 
-//            return new[]
-//                {
-//                    new TestCaseData(noBinding, noBinding, true),
-//                    new TestCaseData(noBinding, category3, false),
-//                    new TestCaseData(noBinding, category1, false),
-//                    new TestCaseData(noBinding, addressCategory3, false),
-//                    new TestCaseData(noBinding, addressCategory1, false),
-//                    new TestCaseData(noBinding, address, false),
+            var anotherCategory3 = new Parameter { Category3Id = 33, Category1Id = 11 };
+            var anotherCategory1 = new Parameter { Category1Id = 11 }; // Не бывает по бизнесу
+            var anotherAddressCategory3 = new Parameter { Category3Id = 33, Category1Id = 11, FirmAddressId = 11 };
+            var anotherAddressCategory1 = new Parameter { Category1Id = 11, FirmAddressId = 11 };
+            var anotherAddress = new Parameter { FirmAddressId = 11 };
 
-//                    new TestCaseData(category3, category3, true),
-//                    new TestCaseData(category3, category1, true),
-//                    new TestCaseData(category3, addressCategory3, true),
-//                    new TestCaseData(category3, addressCategory1, true),
-//                    new TestCaseData(category3, address, false),
+            return new[]
+                {
+                    new TestCaseData(noBinding, noBinding, true),
+                    new TestCaseData(noBinding, category3, false),
+                    new TestCaseData(noBinding, category1, false),
+                    new TestCaseData(noBinding, addressCategory3, false),
+                    new TestCaseData(noBinding, addressCategory1, false),
+                    new TestCaseData(noBinding, address, false),
 
-//                    new TestCaseData(category3, anotherCategory3, false),
-//                    new TestCaseData(category3, anotherCategory1, false),
-//                    new TestCaseData(category3, anotherAddressCategory3, false),
-//                    new TestCaseData(category3, anotherAddressCategory1, false),
-//                    new TestCaseData(category3, anotherAddress, false),
+                    new TestCaseData(category3, category3, true),
+                    new TestCaseData(category3, category1, true),
+                    new TestCaseData(category3, addressCategory3, true),
+                    new TestCaseData(category3, addressCategory1, true),
+                    new TestCaseData(category3, address, false),
 
-//                    new TestCaseData(category1, category1, true),
-//                    new TestCaseData(category1, addressCategory3, true),
-//                    new TestCaseData(category1, addressCategory1, true),
-//                    new TestCaseData(category1, address, false),
+                    new TestCaseData(category3, anotherCategory3, false),
+                    new TestCaseData(category3, anotherCategory1, false),
+                    new TestCaseData(category3, anotherAddressCategory3, false),
+                    new TestCaseData(category3, anotherAddressCategory1, false),
+                    new TestCaseData(category3, anotherAddress, false),
 
-//                    new TestCaseData(category1, anotherCategory1, false),
-//                    new TestCaseData(category1, anotherAddressCategory3, false),
-//                    new TestCaseData(category1, anotherAddressCategory1, false),
-//                    new TestCaseData(category1, anotherAddress, false),
+                    new TestCaseData(category1, category1, true),
+                    new TestCaseData(category1, addressCategory3, true),
+                    new TestCaseData(category1, addressCategory1, true),
+                    new TestCaseData(category1, address, false),
 
-//                    new TestCaseData(addressCategory3, addressCategory3, true),
-//                    new TestCaseData(addressCategory3, addressCategory1, true),
-//                    new TestCaseData(addressCategory3, address, true),
+                    new TestCaseData(category1, anotherCategory1, false),
+                    new TestCaseData(category1, anotherAddressCategory3, false),
+                    new TestCaseData(category1, anotherAddressCategory1, false),
+                    new TestCaseData(category1, anotherAddress, false),
 
-//                    new TestCaseData(addressCategory3, anotherAddressCategory3, false),
-//                    new TestCaseData(addressCategory3, anotherAddressCategory1, false),
-//                    new TestCaseData(addressCategory3, anotherAddress, false),
+                    new TestCaseData(addressCategory3, addressCategory3, true),
+                    new TestCaseData(addressCategory3, addressCategory1, true),
+                    new TestCaseData(addressCategory3, address, true),
 
-//                    new TestCaseData(addressCategory1, addressCategory1, true),
-//                    new TestCaseData(addressCategory1, address, true),
+                    new TestCaseData(addressCategory3, anotherAddressCategory3, false),
+                    new TestCaseData(addressCategory3, anotherAddressCategory1, false),
+                    new TestCaseData(addressCategory3, anotherAddress, false),
 
-//                    new TestCaseData(addressCategory1, anotherAddressCategory1, false),
-//                    new TestCaseData(addressCategory1, anotherAddress, false),
+                    new TestCaseData(addressCategory1, addressCategory1, true),
+                    new TestCaseData(addressCategory1, address, true),
 
-//                    new TestCaseData(address, address, true),
+                    new TestCaseData(addressCategory1, anotherAddressCategory1, false),
+                    new TestCaseData(addressCategory1, anotherAddress, false),
 
-//                    new TestCaseData(address, anotherAddress, false),
+                    new TestCaseData(address, address, true),
 
-//                };
-//        }
+                    new TestCaseData(address, anotherAddress, false),
 
-//        public class Parameter
-//        {
-//            public bool HasNoBinding { get; set; }
-//            public long? Category1Id { get; set; }
-//            public long? Category3Id { get; set; }
-//            public long? FirmAddressId { get; set; }
+                };
+        }
 
-//            public override string ToString()
-//            {
-//                // Для читаемых результатов теста
-//                var sb = new List<string>();
-//                if (Category3Id.HasValue) sb.Add("Рубрика 3-го уровня ");
-//                else if(Category1Id.HasValue) sb.Add("Рубрика 1-го уровня ");
-//                if (FirmAddressId.HasValue) sb.Add("Адрес");
-//                return sb.Any() ? string.Join(" + ", sb) : "Без привязки";
-//            }
-//        }
-//    }
-//}
+        public class Parameter
+        {
+            public bool HasNoBinding { get; set; }
+            public long? Category1Id { get; set; }
+            public long? Category3Id { get; set; }
+            public long? FirmAddressId { get; set; }
+
+            public override string ToString()
+            {
+                // Для читаемых результатов теста
+                var sb = new List<string>();
+                if (Category3Id.HasValue) sb.Add("Рубрика 3-го уровня ");
+                else if (Category1Id.HasValue) sb.Add("Рубрика 1-го уровня ");
+                if (FirmAddressId.HasValue) sb.Add("Адрес");
+                return sb.Any() ? string.Join(" + ", sb) : "Без привязки";
+            }
+        }
+    }
+}
