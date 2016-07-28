feeb05b1f9c094c8463202c8839f6b6e4a9f0858
a.rechkalov <a.rechkalov@2gis.ru>
2016-07-28 17:40:34 +0700

    Добавил транзакцию при чтении

--- ValidationRules/ValidationRules.Replication.Host/ResultDelivery/ResultReadModel.cs
+++ ValidationRules/ValidationRules.Replication.Host/ResultDelivery/ResultReadModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Transactions;
 
 using NuClear.Storage.API.Readings;
 
@@ -19,21 +20,26 @@ namespace NuClear.ValidationRules.Replication.Host.ResultDelivery
 
         public IReadOnlyCollection<Message> GetResults(IEnumerable<Tuple<long, DateTime>> filter)
         {
-            // Просто .First() приводит к сообщению, что подключение закрыто.
-            var lastVersion = _query.For<Version>().OrderByDescending(x => x.Id).Take(1).ToArray().First();
-
-            var results = new List<Message>();
-            foreach (var group in filter.GroupBy(x => x.Item2, x => x.Item1))
+            var transactionOptions = new TransactionOptions { IsolationLevel = IsolationLevel.ReadCommitted, Timeout = TimeSpan.Zero };
+            using (var transaction = new TransactionScope(TransactionScopeOption.Required, transactionOptions))
             {
-                var query = _query.For<Version.ValidationResultByOrder>()
-                                  .Where(x => x.VersionId == lastVersion.Id)
-                                  .Where(x => x.PeriodStart <= group.Key && group.Key < x.PeriodEnd)
-                                  .Where(x => group.Contains(x.OrderId))
-                                  .Select(x => new Message(x.MessageType, x.MessageParams, x.Result));
-                results.AddRange(query);
-            }
+                // Просто .First() приводит к сообщению, что подключение закрыто.
+                var lastVersion = _query.For<Version>().OrderByDescending(x => x.Id).Take(1).ToArray().First();
 
-            return results;
+                var results = new List<Message>();
+                foreach (var group in filter.GroupBy(x => x.Item2, x => x.Item1))
+                {
+                    var query = _query.For<Version.ValidationResultByOrder>()
+                                      .Where(x => x.VersionId == lastVersion.Id)
+                                      .Where(x => x.PeriodStart <= group.Key && group.Key < x.PeriodEnd)
+                                      .Where(x => group.Contains(x.OrderId))
+                                      .Select(x => new Message(x.MessageType, x.MessageParams, x.Result));
+                    results.AddRange(query);
+                }
+
+                transaction.Complete();
+                return results;
+            }
         }
     }
 }
