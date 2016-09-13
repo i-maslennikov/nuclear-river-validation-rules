10b1b840869d60cee706fb549adf08d59d83fe66
a.rechkalov <a.rechkalov@2gis.ru>
2016-09-13 18:23:44 +0700

    Добавил/переименовал параметр конфига

--- ValidationRules.Replication.Host/ResultDelivery/DeliverySettingsAspect.cs
+++ ValidationRules.Replication.Host/ResultDelivery/DeliverySettingsAspect.cs
@@ -6,9 +6,9 @@ namespace NuClear.ValidationRules.Replication.Host.ResultDelivery
     public sealed class ResultDeliverySettingsAspect : ISettingsAspect
     {
         private readonly IntSetting _deleveryHour = ConfigFileSetting.Int.Optional("ResultDeliveryHour", 9);
-        private readonly StringSetting _ermProduction = ConfigFileSetting.String.Required("ErmProduction");
+        private readonly StringSetting _ermAddress = ConfigFileSetting.String.Required("ErmAddress");
 
         public int ResultDeliveryHour => _deleveryHour.Value;
-        public string ErmProduction => _ermProduction.Value;
+        public string ErmAddress => _ermAddress.Value;
     }
 }
