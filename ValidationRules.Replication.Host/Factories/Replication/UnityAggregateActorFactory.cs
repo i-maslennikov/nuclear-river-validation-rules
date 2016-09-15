0c2b265e11344f56c95333ff8176c7000c17d653
a.rechkalov <a.rechkalov@2gis.ru>
2016-09-15 19:00:07 +0700

    Актуализировал ещё один реестр

--- ValidationRules.Replication.Host/Factories/Replication/UnityAggregateActorFactory.cs
+++ ValidationRules.Replication.Host/Factories/Replication/UnityAggregateActorFactory.cs
@@ -6,8 +6,13 @@ using Microsoft.Practices.Unity;
 
 using NuClear.Replication.Core;
 using NuClear.Replication.Core.Actors;
-using NuClear.ValidationRules.Replication.PriceRules.Aggregates;
-using NuClear.ValidationRules.Storage.Model.PriceRules.Aggregates;
+
+using AccountAggregates = NuClear.ValidationRules.Storage.Model.AccountRules.Aggregates;
+using AccountActors = NuClear.ValidationRules.Replication.AccountRules.Aggregates;
+using PriceAggregates = NuClear.ValidationRules.Storage.Model.PriceRules.Aggregates;
+using PriceActors = NuClear.ValidationRules.Replication.PriceRules.Aggregates;
+using ConsistencyAggregates = NuClear.ValidationRules.Storage.Model.ConsistencyRules.Aggregates;
+using ConsistencyActors = NuClear.ValidationRules.Replication.ConsistencyRules.Aggregates;
 
 namespace NuClear.ValidationRules.Replication.Host.Factories.Replication
 {
@@ -18,11 +23,16 @@ namespace NuClear.ValidationRules.Replication.Host.Factories.Replication
         private static readonly IReadOnlyDictionary<Type, Type> AggregateRootActors =
             new Dictionary<Type, Type>
                 {
-                    { typeof(Price), typeof(PriceAggregateRootActor) },
-                    { typeof(Project), typeof(ProjectAggregateRootActor) },
-                    { typeof(Order), typeof(OrderAggregateRootActor) },
-                    { typeof(Period), typeof(PeriodAggregateRootActor) },
-                    { typeof(Position), typeof(PositionAggregateRootActor) },
+                    { typeof(PriceAggregates::Price), typeof(PriceActors::PriceAggregateRootActor) },
+                    { typeof(PriceAggregates::Project), typeof(PriceActors::ProjectAggregateRootActor) },
+                    { typeof(PriceAggregates::Order), typeof(PriceActors::OrderAggregateRootActor) },
+                    { typeof(PriceAggregates::Period), typeof(PriceActors::PeriodAggregateRootActor) },
+                    { typeof(PriceAggregates::Position), typeof(PriceActors::PositionAggregateRootActor) },
+
+                    { typeof(AccountAggregates::Order), typeof(AccountActors::OrderAggregateRootActor) },
+                    { typeof(AccountAggregates::Account), typeof(AccountActors::AccountAggregateRootActor) },
+
+                    { typeof(ConsistencyAggregates::Order), typeof(ConsistencyActors::OrderAggregateRootActor) },
                 };
 
         public UnityAggregateActorFactory(IUnityContainer unityContainer)
