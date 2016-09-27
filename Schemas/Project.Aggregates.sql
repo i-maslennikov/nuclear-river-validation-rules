e0285635180129a6e82f01d1c7474c7339669218
a.rechkalov <a.rechkalov@2gis.ru>
2016-09-27 11:27:09 +0700

    Добавил заготовки под sql-схему

---
+++ Schemas/Project.Aggregates.sql
@@ -0,0 +1,17 @@
+﻿if not exists (select * from sys.schemas where name = 'ProjectAggregates') exec('create schema ProjectAggregates')
+
+if object_id('ProjectAggregates.Order') is not null drop table ProjectAggregates.[Order]
+if object_id('ProjectAggregates.Firm') is not null drop table ProjectAggregates.Firm
+go
+
+create table ProjectAggregates.[Order](
+    Id bigint not null,
+    constraint PK_Order primary key (Id)
+)
+go
+
+create table ProjectAggregates.Firm(
+    Id bigint not null,
+    constraint PK_Firm primary key (Id)
+)
+go
---
+++ Schemas/Project.Facts.sql
@@ -0,0 +1,18 @@
+﻿if not exists (select * from sys.schemas where name = 'ProjectFacts') exec('create schema ProjectFacts')
+go
+
+if object_id('ProjectFacts.Firm') is not null drop table ProjectFacts.Firm
+if object_id('ProjectFacts.[Order]') is not null drop table ProjectFacts.[Order]
+go
+
+create table ProjectFacts.Firm(
+    Id bigint not null,
+    constraint PK_Firm primary key (Id)
+)
+go
+
+create table FirmFacts.[Order](
+    Id bigint not null,
+    constraint PK_Order primary key (Id)
+)
+go
