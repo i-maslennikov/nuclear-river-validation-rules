336db13fa318b3a3e2c55dcff5a30a1ac42d30d8
a.rechkalov <a.rechkalov@2gis.ru>
2016-11-08 15:44:50 +0700

    Добавил api-проект

---
+++ ValidationRules.Querying.Host/Controllers/DefaultController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ValidationRules.Querying.Host.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        // GET: api/Default
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Default/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Default
+        public int Post([FromBody]string value)
+        {
+            return 5;
+        }
+
+        // PUT: api/Default/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Default/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ValidationRules.Querying.Host/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ValidationRules.Querying.Host.WebApiApplication" Language="C#" %>
