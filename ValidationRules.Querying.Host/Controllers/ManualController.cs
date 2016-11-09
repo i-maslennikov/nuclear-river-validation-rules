990ab7ad7cd3bccd5eeb62c51d41ec82579ee7fe
a.rechkalov <a.rechkalov@2gis.ru>
2016-11-09 13:20:28 +0700

    Прикинул будущее api

---
+++ ValidationRules.Querying.Host/Controllers/ManualController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Newtonsoft.Json.Linq;
+
+namespace ValidationRules.Querying.Host.Controllers
+{
+    public class ManualController : ApiController
+    {
+        // POST: api/Manual
+        public int Post([FromBody]ApiRequest request)
+        {
+            //var request = jsonBody.ToObject<Request>();
+            return 5;
+        }
+
+        public class ApiRequest
+        {
+            public IReadOnlyCollection<long> OrderIds { get; set; }
+            public long ProjectId { get; set; }
+            public DateTime ReleaseDate { get; set; }
+            public Guid? State { get; set; }
+        }
+    }
+}
---
+++ ValidationRules.Querying.Host/Controllers/SingleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Newtonsoft.Json.Linq;
+
+namespace ValidationRules.Querying.Host.Controllers
+{
+    public class SingleController : ApiController
+    {
+        // POST: api/Single
+        public int Post([FromBody]ApiRequest request)
+        {
+            //var request = jsonBody.ToObject<Request>();
+            return 5;
+        }
+
+        public class ApiRequest
+        {
+            public long OrderId { get; set; }
+            public Guid? State { get; set; }
+        }
+    }
+}
