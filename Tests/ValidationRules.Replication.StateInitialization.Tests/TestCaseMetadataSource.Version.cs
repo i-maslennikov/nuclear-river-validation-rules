2f476fe6ef51f63a48306df8c05c14d0246f75fe
a.rechkalov <a.rechkalov@2gis.ru>
2016-11-11 18:04:08 +0700

    Добавил тест
    Чтобы StateInitialization знал про тип Version

---
+++ Tests/ValidationRules.Replication.StateInitialization.Tests/TestCaseMetadataSource.Version.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using NuClear.DataTest.Metamodel.Dsl;
+
+using Version = NuClear.ValidationRules.Storage.Model.Messages.Version;
+
+namespace NuClear.ValidationRules.Replication.StateInitialization.Tests
+{
+    public sealed partial class TestCaseMetadataSource
+    {
+        // ReSharper disable once UnusedMember.Local
+        private static ArrangeMetadataElement Version
+        => ArrangeMetadataElement.Config
+            .Name(nameof(Version))
+            .Aggregate()
+            .Message(new Version {Id = 0, Date = DateTime.UtcNow})
+            .Ignored();
+    }
+}
