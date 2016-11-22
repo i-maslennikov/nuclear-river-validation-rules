5c017aa273b768207de7150ce0ed643b2f31c70e
Пашук Максим Владимирович <m.pashuk@2gis.ru>
2016-11-22 18:40:27 +0700

    fixes in erm subdomain

--- ValidationRules.Replication/EntityTypeIds.cs
+++ ValidationRules.Replication/EntityTypeIds.cs
@@ -11,9 +11,13 @@
         public const int Category = 160;
         public const int CategoryFirmAddress = 166;
         public const int DeniedPosition = 180;
+        public const int Deal = 199;
         public const int Firm = 146;
+        public const int BranchOffice = 156;
+        public const int BranchOfficeOrganizationUnit = 139;
         public const int FirmAddress = 164;
         public const int FirmContact = 165;
+        public const int LegalPerson = 147;
         public const int LegalPersonProfile = 219;
         public const int Lock = 159;
         public const int Order = 151;
