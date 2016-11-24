6ef6ab7caf595ec3dd3ae336b7e32b31c6f08306
a.rechkalov <a.rechkalov@2gis.ru>
2016-11-24 16:24:59 +0700

    Вычистил Debug.WriteLine

--- ValidationRules.Replication.Host/DI/CachingTelemetryPublisherDecorator.cs
+++ ValidationRules.Replication.Host/DI/CachingTelemetryPublisherDecorator.cs
@@ -30,7 +30,6 @@ namespace NuClear.ValidationRules.Replication.Host.DI
         public void Publish<T>(long value)
             where T : TelemetryIdentityBase<T>, new()
         {
-            Debug.WriteLine($"{typeof(T).Name}: +{value}");
             _dictionary.AddOrUpdate(_publisher.Publish<T>, value, (action, accumulated) => accumulated + value);
         }
 
@@ -45,44 +44,35 @@ namespace NuClear.ValidationRules.Replication.Host.DI
             {
                 try
                 {
-                    Debug.WriteLine($"flush begin");
                     foreach (var key in _dictionary.Keys)
                     {
                         long value;
                         if (_dictionary.TryRemove(key, out value))
                         {
-                            Debug.WriteLine($"flush {value}");
                             key.Invoke(value);
                         }
                     }
-                    Debug.WriteLine($"flush ok");
 
                     Task.Delay(500, token);
                 }
                 catch (Exception exception)
                 {
-                    Debug.WriteLine($"flush error");
                     _tracer.Error(exception, "Ошибка при асинхронной отправке телеметрии");
                     Task.Delay(5000, token);
                 }
             }
-
-            Debug.WriteLine($"flush completed");
         }
 
         public void Dispose()
         {
-            Debug.WriteLine($"disposing...");
             _cancellationTokenSource.Cancel(false);
 
             try
             {
                 _flusher.Wait();
-                Debug.WriteLine($"disposing ok");
             }
             catch (Exception ex)
             {
-                Debug.WriteLine($"disposing error");
                 _tracer.Error(ex, "Ошибка завершения асинхронной отправке телеметрии");
             }
         }
