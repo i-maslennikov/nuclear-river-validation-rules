3f3122c0f91e01cd1da04836c4b216f6dfa3f67e
a.rechkalov <a.rechkalov@2gis.ru>
2016-11-24 17:26:43 +0700

    Добавил todo

--- ValidationRules.Replication.Host/DI/EmptyOperationRegistry.cs
+++ ValidationRules.Replication.Host/DI/EmptyOperationRegistry.cs
@@ -3,6 +3,7 @@ using NuClear.Replication.OperationsProcessing.Primary;
 
 namespace NuClear.ValidationRules.Replication.Host.DI
 {
+    //todo: Проверь наличие такого в NuClear.Replication.OperationsProcessing.Primary. Если уже есть, удали этот.
     public sealed class EmptyOperationRegistry<T> : IOperationRegistry<T>
     {
         public bool IsAllowedOperation(StrictOperationIdentity operationIdentity)
