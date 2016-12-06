8b8a41c9282275d06311cb30d4ec406fe5e6875b
a.rechkalov <a.rechkalov@2gis.ru>
2016-12-06 11:39:19 +0700

    Исправил регистрацию value-object
    Факты, зарегистрированные без указания PK пересчитываются
    некорректно, это связано с двухэтапным DataChangesDetector:
    сравнение по идентификатору без идентификатора приводит к тому,
    что все объекты считаются одинаковыми.
    
    Это можно пофиксить в river полным сравнением в случае
    отсутствия идентификатора, но надо подумать.
    Возможно будет лучше для identityComparer создавать такой comparer,
    что будет возвращать false для любой пары объектов.
    Или использовать completeComparer.

--- ValidationRules.Storage/Schema.Facts.cs
+++ ValidationRules.Storage/Schema.Facts.cs
@@ -24,26 +24,26 @@ namespace NuClear.ValidationRules.Storage
         private static FluentMappingBuilder RegisterFacts(this FluentMappingBuilder builder)
         {
             builder.Entity<Account>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<Advertisement>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<AdvertisementElement>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<AdvertisementElementTemplate>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<AdvertisementTemplate>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<AssociatedPosition>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<AssociatedPositionsGroup>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<Bargain>()
                    .HasSchemaName(FactsSchema)
                    .HasPrimaryKey(x => x.Id);
@@ -60,31 +60,34 @@ namespace NuClear.ValidationRules.Storage
                    .HasSchemaName(FactsSchema)
                    .HasPrimaryKey(x => x.Id);
             builder.Entity<Category>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<CategoryOrganizationUnit>()
                    .HasSchemaName(FactsSchema)
                    .HasPrimaryKey(x => x.Id);
             builder.Entity<CostPerClickCategoryRestriction>()
-                   .HasSchemaName(FactsSchema);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.ProjectId)
+                   .HasPrimaryKey(x => x.CategoryId)
+                   .HasPrimaryKey(x => x.Begin);
             builder.Entity<Deal>()
                    .HasSchemaName(FactsSchema)
                    .HasPrimaryKey(x => x.Id);
             builder.Entity<DeniedPosition>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<Firm>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<FirmAddress>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<FirmAddressCategory>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<FirmAddressWebsite>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<LegalPerson>()
                    .HasSchemaName(FactsSchema)
                    .HasPrimaryKey(x => x.Id);
@@ -92,59 +95,71 @@ namespace NuClear.ValidationRules.Storage
                    .HasSchemaName(FactsSchema)
                    .HasPrimaryKey(x => x.Id);
             builder.Entity<Lock>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<Order>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<OrderPosition>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<OrderPositionAdvertisement>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<OrderPositionCostPerClick>()
-                   .HasSchemaName(FactsSchema);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.OrderPositionId)
+                   .HasPrimaryKey(x => x.CategoryId);
             builder.Entity<OrderScanFile>()
                    .HasSchemaName(FactsSchema)
                    .HasPrimaryKey(x => x.Id);
             builder.Entity<Position>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<PositionChild>()
-                  .HasSchemaName(FactsSchema);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.MasterPositionId)
+                   .HasPrimaryKey(x => x.ChildPositionId);
             builder.Entity<Price>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<PricePosition>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<Project>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<ReleaseInfo>()
                    .HasSchemaName(FactsSchema)
                    .HasPrimaryKey(x => x.Id);
             builder.Entity<ReleaseWithdrawal>()
-              .HasSchemaName(FactsSchema)
-              .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<RulesetRule>()
-                  .HasSchemaName(FactsSchema);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.RulesetId)
+                   .HasPrimaryKey(x => x.RuleType)
+                   .HasPrimaryKey(x => x.DependentPositionId)
+                   .HasPrimaryKey(x => x.PrincipalPositionId)
+                   .HasPrimaryKey(x => x.ObjectBindingType);
             builder.Entity<SalesModelCategoryRestriction>()
-                   .HasSchemaName(FactsSchema);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.ProjectId)
+                   .HasPrimaryKey(x => x.CategoryId)
+                   .HasPrimaryKey(x => x.Begin);
             builder.Entity<Theme>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<ThemeCategory>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<ThemeOrganizationUnit>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.Id);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.Id);
             builder.Entity<UnlimitedOrder>()
-                  .HasSchemaName(FactsSchema)
-                  .HasPrimaryKey(x => x.OrderId)
-                  .HasPrimaryKey(x => x.PeriodStart);
+                   .HasSchemaName(FactsSchema)
+                   .HasPrimaryKey(x => x.OrderId)
+                   .HasPrimaryKey(x => x.PeriodStart);
             return builder;
         }
     }
