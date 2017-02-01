079deaf31e4116a6ecd4b1ebf59de4e0756c21b8
a.rechkalov <a.rechkalov@2gis.ru>
2017-02-01 12:15:30 +0700

    Исправил проверку баланса для заказов со 100% скидкой
    Ошибка не должна выводиться для заказов, по лицевым счетам которых
    не планируется списаний, даже если есть задолженность.
    
    Похоже, что если списания планируются - то ошибка должна появиться
    даже в заказах со скидкой.

--- ValidationRules.Replication/AccountRules/Validation/AccountBalanceShouldBePositive.cs
+++ ValidationRules.Replication/AccountRules/Validation/AccountBalanceShouldBePositive.cs
@@ -37,7 +37,7 @@ namespace NuClear.ValidationRules.Replication.AccountRules.Validation
             var nonFreeOfChargeOrders = query.For<Order>().Where(x => !x.IsFreeOfCharge);
 
             var ruleResults =
-                from accountPeriod in query.For<Account.AccountPeriod>()
+                from accountPeriod in query.For<Account.AccountPeriod>().Where(x => x.ReleaseAmount > 0)
                 join order in nonFreeOfChargeOrders on accountPeriod.AccountId equals order.AccountId
                 where order.BeginDistributionDate < accountPeriod.End && accountPeriod.Start < order.EndDistributionDate
                 where accountPeriod.Balance - accountPeriod.ReleaseAmount - (accountPeriod.OwerallLockedAmount - accountPeriod.LockedAmount) <= -Epsilon
