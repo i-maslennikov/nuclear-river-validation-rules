3739787e0127b6f498b3ff1743fd91db2b54e4e9
a.rechkalov <a.rechkalov@2gis.ru>
2017-03-27 15:45:59 +0700

    Исправил скрипт с WebApp.Lock
    Таблица не должна пересоздаваться

--- Schemas/WebApp.sql
+++ Schemas/WebApp.sql
@@ -1,14 +1,13 @@
 ﻿if not exists (select * from sys.schemas where name = 'WebApp') exec('create schema WebApp')
 go
 
-if object_id('WebApp.Lock') is not null drop table WebApp.Lock
+if object_id('WebApp.Lock') is null
+    create table WebApp.Lock(
+        Id int not null,
+        Expires datetime2(2) not null,
+        InUse bit not null,
+        IsNew bit not null,
 
-create table WebApp.Lock(
-    Id int not null,
-    Expires datetime2(2) not null,
-    InUse bit not null,
-    IsNew bit not null,
-
-    constraint [PK_Lock] primary key (Id)
-)
+        constraint [PK_Lock] primary key (Id)
+    )
 go
