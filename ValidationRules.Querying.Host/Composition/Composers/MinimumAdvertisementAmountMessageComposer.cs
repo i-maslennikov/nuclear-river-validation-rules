bf8df6d667ccd9958f3b282e0deefa62fd2f3c90
a.rechkalov <a.rechkalov@2gis.ru>
2017-03-29 15:21:32 +0700

    Исправил вывод сообщений о количестве рекламы
    Erm в единичном режиме выводит отдельно для каждого месяца.
    У нас же DefaultDistinctor слеплял их в одно, поэтому нужно не использовать его.

--- ValidationRules.Querying.Host/Composition/Composers/MinimumAdvertisementAmountMessageComposer.cs
+++ ValidationRules.Querying.Host/Composition/Composers/MinimumAdvertisementAmountMessageComposer.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
 
+using NuClear.ValidationRules.Querying.Host.DataAccess;
 using NuClear.ValidationRules.Querying.Host.Properties;
 using NuClear.ValidationRules.Storage.Identitites.EntityTypes;
 using NuClear.ValidationRules.Storage.Model.Messages;
 
 namespace NuClear.ValidationRules.Querying.Host.Composition.Composers
 {
-    public sealed class MinimumAdvertisementAmountMessageComposer : IMessageComposer
+    public sealed class MinimumAdvertisementAmountMessageComposer : IMessageComposer, IDistinctor
     {
         public MessageTypeCode MessageType => MessageTypeCode.MinimumAdvertisementAmount;
 
@@ -25,5 +26,8 @@ namespace NuClear.ValidationRules.Querying.Host.Composition.Composers
                     dto.Month,
                     dto.Count));
         }
+
+        public IEnumerable<Message> Distinct(IEnumerable<Message> messages)
+            => messages;
     }
 }
