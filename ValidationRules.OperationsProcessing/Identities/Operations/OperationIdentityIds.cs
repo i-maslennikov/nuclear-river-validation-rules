aec5802990d51d0bc3ff0efcfc3557a5e74afeba
a.rechkalov <a.rechkalov@2gis.ru>
2017-04-13 17:14:51 +0700

    Удалил неиспользуемые OperationIdentity

--- ValidationRules.OperationsProcessing/Identities/Operations/OperationIdentityIds.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NuClear.ValidationRules.OperationsProcessing.Identities.Operations
-{
-    public static class OperationIdentityIds
-    {
-        public const int ManageDraftRulesetIdentity = 28000;
-        public const int PublishRulesetIdentity = 28001;
-        public const int UnpublishRulesetIdentity = 28002;
-    }
-}
