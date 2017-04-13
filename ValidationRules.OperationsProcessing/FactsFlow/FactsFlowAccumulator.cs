1cdf271c3334016df1b598c19d8b33f3e58ce850
a.rechkalov <a.rechkalov@2gis.ru>
2017-04-13 17:24:47 +0700

    Упростил FactsFlowAccumulator
    Ибо создаётся/вызывается в больших количествах.

--- ValidationRules.OperationsProcessing/FactsFlow/FactsFlowAccumulator.cs
+++ ValidationRules.OperationsProcessing/FactsFlow/FactsFlowAccumulator.cs
@@ -6,39 +6,25 @@ using NuClear.Messaging.API.Processing.Actors.Accumulators;
 using NuClear.OperationsTracking.API.UseCases;
 using NuClear.Replication.Core;
 using NuClear.Replication.OperationsProcessing;
-using NuClear.Replication.OperationsProcessing.Telemetry;
-using NuClear.Telemetry;
-using NuClear.Tracing.API;
 using NuClear.ValidationRules.Replication.Commands;
 
 namespace NuClear.ValidationRules.OperationsProcessing.FactsFlow
 {
     public sealed class FactsFlowAccumulator : MessageProcessingContextAccumulatorBase<FactsFlow, TrackedUseCase, AggregatableMessage<ICommand>>
     {
-        private readonly ITracer _tracer;
-        private readonly ITelemetryPublisher _telemetryPublisher;
         private readonly ICommandFactory _commandFactory;
 
-        public FactsFlowAccumulator(ITracer tracer,
-                                             ITelemetryPublisher telemetryPublisher)
+        public FactsFlowAccumulator()
         {
-            _tracer = tracer;
-            _telemetryPublisher = telemetryPublisher;
             _commandFactory = new ImportFactsFromErmCommandFactory();
         }
 
         protected override AggregatableMessage<ICommand> Process(TrackedUseCase trackedUseCase)
         {
-            _tracer.DebugFormat("Processing TUC {0}", trackedUseCase.Id);
-
-            _telemetryPublisher.Publish<ErmReceivedOperationCountIdentity>(1);
-
             var commands = _commandFactory.CreateCommands(new ImportFactsFromErmEvent(trackedUseCase)).ToList();
 
             commands.Add(new IncrementStateCommand(new[] { trackedUseCase.Id }));
             commands.Add(new LogDelayCommand(trackedUseCase.Context.Finished.UtcDateTime));
-
-            _telemetryPublisher.Publish<ErmEnqueuedOperationCountIdentity>(commands.Count);
 
             return new AggregatableMessage<ICommand>
             {
