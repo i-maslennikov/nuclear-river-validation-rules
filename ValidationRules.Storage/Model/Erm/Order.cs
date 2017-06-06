5ca3353b567f6115fa5332f6e82427249034ca2a
a.rechkalov <a.rechkalov@2gis.ru>
2017-06-06 14:05:05 +0700

    Добавил тип бесплатного заказа "служебный"
    ERM-10865

--- ValidationRules.Storage/Model/Erm/Order.cs
+++ ValidationRules.Storage/Model/Erm/Order.cs
@@ -8,8 +8,9 @@ namespace NuClear.ValidationRules.Storage.Model.Erm
         public const int OrderTypeSocialAds = 7;
         public const int OrderTypeCompensation = 9;
         public const int OrderTypeInternational = 10;
+        public const int OrderTypeService = 11;
 
-        public static readonly int[] FreeOfChargeTypes = { OrderTypeSelfAds, OrderTypeSocialAds, OrderTypeCompensation, OrderTypeInternational };
+        public static readonly int[] FreeOfChargeTypes = { OrderTypeSelfAds, OrderTypeSocialAds, OrderTypeCompensation, OrderTypeInternational, OrderTypeService };
 
         private const int OrderStateArchive = 6;
         private const int OrderStateRejected = 3;
