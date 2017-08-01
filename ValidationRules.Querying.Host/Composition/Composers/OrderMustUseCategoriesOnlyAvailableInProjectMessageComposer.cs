e18da0b27531ab7dfc8e1c8e6db75d7770842050
Пашук Максим Владимирович <m.pashuk@2gis.ru>
2017-08-01 17:36:06 +0700

    Сделал правило привязки рубрик к городу таким же как в ERM
    У нас в метаданных вообще стоял не тот код проверки (7 вместо 8), и мы сравнивали хз что с чем. Сейчас вроде то что надо. Сделал по аналогии с проверкой на купоны.

--- ValidationRules.Querying.Host/Composition/Composers/OrderMustUseCategoriesOnlyAvailableInProjectMessageComposer.cs
+++ ValidationRules.Querying.Host/Composition/Composers/OrderMustUseCategoriesOnlyAvailableInProjectMessageComposer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 using NuClear.ValidationRules.Querying.Host.Properties;
 using NuClear.ValidationRules.Storage.Identitites.EntityTypes;
@@ -6,21 +7,51 @@ using NuClear.ValidationRules.Storage.Model.Messages;
 
 namespace NuClear.ValidationRules.Querying.Host.Composition.Composers
 {
-    public sealed class OrderMustUseCategoriesOnlyAvailableInProjectMessageComposer : IMessageComposer
+    public sealed class OrderMustUseCategoriesOnlyAvailableInProjectMessageComposer : IMessageComposer, IDistinctor
     {
         public MessageTypeCode MessageType => MessageTypeCode.OrderMustUseCategoriesOnlyAvailableInProject;
 
         public MessageComposerResult Compose(NamedReference[] references, IReadOnlyDictionary<string, string> extra)
         {
             var orderReference = references.Get<EntityTypeOrder>();
-            var categoryReference = references.Get<EntityTypeCategory>();
             var orderPositionReference = references.Get<EntityTypeOrderPosition>();
+            // todo: сортировки нет в требованиях, сделана для соответствия erm
+            var categoryReferences = references.GetMany<EntityTypeCategory>().OrderBy(x => x.Name);
 
             return new MessageComposerResult(
                 orderReference,
-                Resources.OrdersCheckOrderPositionContainsCategoriesFromWrongOrganizationUnit,
-                categoryReference,
-                orderPositionReference);
+                string.Format(Resources.OrderCheckOrderPositionContainsCategoriesFromWrongOrganizationUnit, string.Join(", ", categoryReferences.Select((x, i) => "{" + (i + 1) + "}"))),
+                new []{ orderPositionReference }.Concat(categoryReferences).ToArray());
+        }
+
+        public IEnumerable<Message> Distinct(IEnumerable<Message> messages)
+        {
+            return messages.GroupBy(x =>
+            {
+                var opaReference = x.References.Get<EntityTypeOrderPositionAdvertisement>();
+
+                return new
+                {
+                    x.OrderId,
+                    x.MessageType,
+                    x.ProjectId,
+                    OrderPositionId = opaReference.Children.Get<EntityTypeOrderPosition>().Id,
+                    PositionId = opaReference.Children.Get<EntityTypePosition>().Id
+                };
+            }, x => x.References)
+            .Select(x => new Message
+            {
+                OrderId = x.Key.OrderId,
+                MessageType = x.Key.MessageType,
+                ProjectId = x.Key.ProjectId,
+                References = new List<Reference>(x.SelectMany(ReferenceExtensions.GetMany<EntityTypeCategory>))
+                {
+                    new Reference(EntityTypeOrder.Instance.Id, x.Key.OrderId.Value),
+                    new Reference(EntityTypeOrderPositionAdvertisement.Instance.Id, 0,
+                                    new Reference(EntityTypeOrderPosition.Instance.Id, x.Key.OrderPositionId),
+                                    new Reference(EntityTypePosition.Instance.Id, x.Key.PositionId))
+                }
+            });
         }
     }
 }
