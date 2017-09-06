aa7ecaa0d938e8e9af4633321b56bae87b70de41
a.rechkalov <a.rechkalov@2gis.ru>
2017-09-06 18:32:16 +0700

    Указал культуру в тестовой web-морде
    Захардкодил "ru", поскольку не думаю, что кто-то ещё будет им пользоваться.

--- ValidationRules.WebApp/src/DataAccess/QueryingClient.cs
+++ ValidationRules.WebApp/src/DataAccess/QueryingClient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,48 +28,48 @@ namespace NuClear.ValidationRules.WebApp.DataAccess
 
         public async Task<IReadOnlyCollection<ValidationResult>> Single(long orderId)
         {
-            var httpContent = new StringContent(JsonConvert.SerializeObject(new { OrderId = orderId }), Encoding.UTF8, "application/json");
-            var response = await HttpClient.PostAsync(_settings.Single, httpContent);
-            response.EnsureSuccessStatusCode();
-            var responseString = await response.Content.ReadAsStringAsync();
-            var result = JsonConvert.DeserializeObject<IReadOnlyCollection<ValidationResult>>(responseString);
+            var request = CreateRequest(_settings.Single, new { OrderId = orderId });
+            var result = await Execute(request);
             return result;
         }
 
         public async Task<IEnumerable<ValidationResult>> Manual(IReadOnlyCollection<long> orderIds, DateTime date, long? projectId)
         {
-            var httpContent = new StringContent(JsonConvert.SerializeObject(new { OrderIds = orderIds, ReleaseDate = date, ProjectId = projectId }),
-                                                Encoding.UTF8,
-                                                "application/json");
-            var response = await HttpClient.PostAsync(_settings.Manual, httpContent);
-            response.EnsureSuccessStatusCode();
-            var responseString = await response.Content.ReadAsStringAsync();
-            var result = JsonConvert.DeserializeObject<IReadOnlyCollection<ValidationResult>>(responseString);
+            var request = CreateRequest(_settings.Manual, new { OrderIds = orderIds, ReleaseDate = date, ProjectId = projectId });
+            var result = await Execute(request);
             return result;
         }
 
         public async Task<IEnumerable<ValidationResult>> Prerelease(IReadOnlyCollection<long> orderIds, DateTime date, long projectId)
         {
-            var httpContent = new StringContent(JsonConvert.SerializeObject(new { OrderIds = orderIds, ReleaseDate = date, ProjectId = projectId }),
-                                                Encoding.UTF8,
-                                                "application/json");
-            var response = await HttpClient.PostAsync(_settings.Prerelease, httpContent);
-            response.EnsureSuccessStatusCode();
-            var responseString = await response.Content.ReadAsStringAsync();
-            var result = JsonConvert.DeserializeObject<IReadOnlyCollection<ValidationResult>>(responseString);
+            var request = CreateRequest(_settings.Prerelease, new { OrderIds = orderIds, ReleaseDate = date, ProjectId = projectId });
+            var result = await Execute(request);
             return result;
         }
 
         public async Task<IEnumerable<ValidationResult>> Release(IReadOnlyCollection<long> orderIds, DateTime date, long projectId)
         {
-            var httpContent = new StringContent(JsonConvert.SerializeObject(new { OrderIds = orderIds, ReleaseDate = date, ProjectId = projectId }),
-                                                Encoding.UTF8,
-                                                "application/json");
-            var response = await HttpClient.PostAsync(_settings.Release, httpContent);
+            var request = CreateRequest(_settings.Release, new { OrderIds = orderIds, ReleaseDate = date, ProjectId = projectId });
+            var result = await Execute(request);
+            return result;
+        }
+
+        private static HttpRequestMessage CreateRequest(Uri uri, object payload)
+            => new HttpRequestMessage(HttpMethod.Post, uri)
+                {
+                    Content = new StringContent(
+                        JsonConvert.SerializeObject(payload),
+                        Encoding.UTF8,
+                        "application/json"),
+                    Headers = { AcceptLanguage = { new StringWithQualityHeaderValue("ru") } }
+                };
+
+        private static async Task<IReadOnlyCollection<ValidationResult>> Execute(HttpRequestMessage request)
+        {
+            var response = await HttpClient.SendAsync(request);
             response.EnsureSuccessStatusCode();
             var responseString = await response.Content.ReadAsStringAsync();
-            var result = JsonConvert.DeserializeObject<IReadOnlyCollection<ValidationResult>>(responseString);
-            return result;
+            return JsonConvert.DeserializeObject<IReadOnlyCollection<ValidationResult>>(responseString);
         }
     }
 }
