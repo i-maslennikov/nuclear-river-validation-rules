185ad004e67a46cd08242bb4f48e56e0bb8214c9
a.rechkalov <a.rechkalov@2gis.ru>
2017-10-26 11:01:51 +0700

    Удалил кеш
    Чтобы освободить память. Кеш всё-равно нужно выносить из процесса.

--- ValidationRules.Replication/Messages/ValidationResultCache.cs
+++ ValidationRules.Replication/Messages/ValidationResultCache.cs
@@ -7,7 +7,6 @@ namespace NuClear.ValidationRules.Replication.Messages
 {
     internal sealed class ValidationResultCache
     {
-        private CacheRecord _record;
         private static readonly Lazy<ValidationResultCache> Singleton = new Lazy<ValidationResultCache>(() => new ValidationResultCache());
 
         private ValidationResultCache()
@@ -18,36 +17,16 @@ namespace NuClear.ValidationRules.Replication.Messages
 
         public bool TryGet(long key, out IReadOnlyCollection<Version.ValidationResult> value)
         {
-            var record = _record;
-            value = record?.Key == key ? record.Value : null;
-            return value != null;
+            value = null;
+            return false;
         }
 
         public void Put(long key, IReadOnlyCollection<Version.ValidationResult> value)
         {
-            _record = new CacheRecord(key, new HashSet<Version.ValidationResult>(value));
         }
 
         public void ApplyPatch(IReadOnlyCollection<Version.ValidationResult> added, IReadOnlyCollection<Version.ValidationResult> removed)
         {
-            var record = _record;
-            var patched = new HashSet<Version.ValidationResult>(record.Value);
-            patched.UnionWith(added);
-            patched.ExceptWith(removed);
-            _record = new CacheRecord(record.Key + 1, patched);
-        }
-
-        private class CacheRecord
-        {
-            public CacheRecord(long key, IReadOnlyCollection<Version.ValidationResult> value)
-            {
-                Key = key;
-                Value = value;
-            }
-
-            public long Key { get; }
-
-            public IReadOnlyCollection<Version.ValidationResult> Value { get; }
         }
     }
 }
