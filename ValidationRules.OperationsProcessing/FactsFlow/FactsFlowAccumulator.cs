5f4da4373265422be6adbefb76623132937759e4
a.rechkalov <a.rechkalov@2gis.ru>
2017-10-31 15:05:23 +0700

    Отделил CommandFactory типы

--- ValidationRules.OperationsProcessing/FactsFlow/FactsFlowAccumulator.cs
+++ ValidationRules.OperationsProcessing/FactsFlow/FactsFlowAccumulator.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Transactions;
@@ -10,8 +9,6 @@ using NuClear.Replication.Core;
 using NuClear.Replication.OperationsProcessing;
 using NuClear.Storage.API.Readings;
 using NuClear.Tracing.API;
-using NuClear.ValidationRules.Replication;
-using NuClear.ValidationRules.Replication.Commands;
 using NuClear.ValidationRules.Storage.Model.Erm;
 
 namespace NuClear.ValidationRules.OperationsProcessing.FactsFlow
@@ -60,36 +57,6 @@ namespace NuClear.ValidationRules.OperationsProcessing.FactsFlow
             }
 
             _tracer.Warn($"Ignored TUC {id} after {TotalWaitMilliseconds}ms waiting");
-        }
-
-        private sealed class FactsCommandFactory : ICommandFactory<TrackedUseCase>
-        {
-            public IReadOnlyCollection<ICommand> CreateCommands(TrackedUseCase message)
-            {
-                var changes = message.Operations.SelectMany(x => x.AffectedEntities.Changes);
-                return changes.SelectMany(x => CommandsForEntityType(x.Key.Id, x.Value.Keys)).Concat(CreateIncrementErmStateCommand(message)).ToList();
-            }
-
-            private static IEnumerable<ICommand> CreateIncrementErmStateCommand(TrackedUseCase message)
-            {
-                return new[] { new IncrementErmStateCommand(new[] { new ErmState(message.Id, message.Context.Finished.UtcDateTime) }) };
-            }
-
-            private static IEnumerable<ICommand> CommandsForEntityType(int entityTypeId, IEnumerable<long> ids)
-            {
-                var commands = Enumerable.Empty<ICommand>();
-
-                if (EntityTypeMap.TryGetFactTypes(entityTypeId, out var factTypes))
-                {
-                    var syncDataObjectCommands = from factType in factTypes
-                                                 from id in ids
-                                                 select new SyncDataObjectCommand(factType, id);
-
-                    commands = commands.Concat(syncDataObjectCommands);
-                }
-
-                return commands;
-            }
         }
     }
 }
--- ValidationRules.OperationsProcessing/MessagesFlow/MessagesFlowAccumulator.cs
+++ ValidationRules.OperationsProcessing/MessagesFlow/MessagesFlowAccumulator.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using NuClear.Messaging.API.Processing.Actors.Accumulators;
+﻿using NuClear.Messaging.API.Processing.Actors.Accumulators;
 using NuClear.Replication.Core;
 using NuClear.Replication.OperationsProcessing;
-using NuClear.ValidationRules.Replication.Commands;
-using NuClear.ValidationRules.Replication.Events;
 
 namespace NuClear.ValidationRules.OperationsProcessing.MessagesFlow
 {
@@ -26,33 +20,6 @@ namespace NuClear.ValidationRules.OperationsProcessing.MessagesFlow
                 TargetFlow = MessageFlow,
                 Commands = _commandFactory.CreateCommands(message)
             };
-        }
-
-        private sealed class MessagesFlowCommandFactory : ICommandFactory<EventMessage>
-        {
-            public IReadOnlyCollection<ICommand> CreateCommands(EventMessage message)
-            {
-                switch (message.Event)
-                {
-                    case AmsStateIncrementedEvent amsStateIncrementedEvent:
-                        return new[] { new StoreAmsStateCommand(amsStateIncrementedEvent.State) };
-
-                    case ErmStateIncrementedEvent ermStateIncrementedEvent:
-                        return new[] { new StoreErmStateCommand(ermStateIncrementedEvent.States) };
-
-                    case DelayLoggedEvent delayLoggedEvent:
-                        return new[] { new LogDelayCommand(delayLoggedEvent.EventTime) };
-
-                    case ResultOutdatedEvent resultOutdatedEvent:
-                        return new[] { new RecalculateValidationRuleCompleteCommand(resultOutdatedEvent.Rule) };
-
-                    case ResultPartiallyOutdatedEvent resultPartiallyOutdatedEvent:
-                        return new[] { new RecalculateValidationRulePartiallyCommand(resultPartiallyOutdatedEvent.Rule, resultPartiallyOutdatedEvent.OrderIds) };
-
-                    default:
-                        throw new ArgumentException($"Unexpected event '{message}'", nameof(message));
-                }
-            }
         }
     }
 }
