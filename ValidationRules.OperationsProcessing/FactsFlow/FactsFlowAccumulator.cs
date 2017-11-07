26740f7b9f165b261652d529d4ce69aa3d74c631
a.rechkalov <a.rechkalov@2gis.ru>
2017-11-07 16:20:27 +0700

    Удалил неиспользуемое

--- ValidationRules.OperationsProcessing/FactsFlow/FactsFlowAccumulator.cs
+++ ValidationRules.OperationsProcessing/FactsFlow/FactsFlowAccumulator.cs
@@ -1,30 +1,16 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Transactions;
-
-using NuClear.Messaging.API.Processing.Actors.Accumulators;
+﻿using NuClear.Messaging.API.Processing.Actors.Accumulators;
 using NuClear.OperationsTracking.API.UseCases;
 using NuClear.Replication.Core;
 using NuClear.Replication.OperationsProcessing;
-using NuClear.Storage.API.Readings;
-using NuClear.Tracing.API;
-using NuClear.ValidationRules.Storage.Model.Erm;
 
 namespace NuClear.ValidationRules.OperationsProcessing.FactsFlow
 {
     public sealed class FactsFlowAccumulator : MessageProcessingContextAccumulatorBase<FactsFlow, TrackedUseCase, AggregatableMessage<ICommand>>
     {
-        private const int TotalWaitMilliseconds = 60000;
-
-        private readonly IQuery _query;
-        private readonly ITracer _tracer;
         private readonly ICommandFactory<TrackedUseCase> _commandFactory;
 
-        public FactsFlowAccumulator(IQuery query, ITracer tracer)
+        public FactsFlowAccumulator()
         {
-            _query = query;
-            _tracer = tracer;
             _commandFactory = new FactsCommandFactory();
         }
 
