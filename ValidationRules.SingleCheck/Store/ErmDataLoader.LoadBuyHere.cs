469d2a3961b88136f778ecda3e5cfd888875a53c
a.rechkalov <a.rechkalov@2gis.ru>
2017-11-20 17:56:28 +0700

    Актуализировад данные для единичной проверки

---
+++ ValidationRules.SingleCheck/Store/ErmDataLoader.LoadBuyHere.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using LinqToDB.Data;
+
+using NuClear.ValidationRules.Storage.Model.Erm;
+
+namespace NuClear.ValidationRules.SingleCheck.Store
+{
+    public static partial class ErmDataLoader
+    {
+
+        private static void LoadBuyHere(DataConnection query, Order order, IStore store)
+        {
+            const long CategoryCodePremiumAdvertising = 809065011136692321; // Реклама в профилях партнеров (приоритетное размещение)
+            const long CategoryCodeAdvertisingAddress = 809065011136692326; // Реклама в профилях партнеров (адреса)
+            var categoryCodes = new[] { CategoryCodePremiumAdvertising, CategoryCodeAdvertisingAddress };
+
+            var firmAddresses = (
+                from op in query.GetTable<OrderPosition>().Where(x => x.IsActive && !x.IsDeleted).Where(x => x.OrderId == order.Id)
+                from opa in query.GetTable<OrderPositionAdvertisement>().Where(x => x.OrderPositionId == op.Id)
+                from position in query.GetTable<Position>().Where(x => x.CategoryCode == CategoryCodeAdvertisingAddress).Where(x => x.Id == opa.PositionId)
+                from address in query.GetTable<FirmAddress>().Where(x => x.Id == opa.FirmAddressId)
+                select address
+            ).Execute();
+            store.AddRange(firmAddresses);
+            var firmAddressIds = firmAddresses.Select(x => x.Id).ToList();
+            var firmIds = firmAddresses.Select(x => x.FirmId).Distinct().ToList();
+
+            if (!firmAddresses.Any())
+            {
+                return;
+            }
+
+            // Нужны заказы этих фирм - вдруг фирма является рекламодателем.
+            var firmOrders = query.GetTable<Order>()
+                .Where(x => firmIds.Contains(x.FirmId))
+                .Where(x => new[] { 2, 4, 5 }.Contains(x.WorkflowStepId))
+                .Where(x => x.BeginDistributionDate < order.EndDistributionDatePlan && order.BeginDistributionDate < x.EndDistributionDatePlan)
+                .Execute();
+            store.AddRange(firmOrders);
+
+            // Нужны другие ЗМК заказы на те же самые адреса
+            var positions =
+                query.GetTable<Position>()
+                    .Where(x => categoryCodes.Contains(x.CategoryCode))
+                    .Execute();
+            store.AddRange(positions);
+            var positionIds = positions.Select(x => x.Id).ToList();
+
+            var xxxOrders =
+                from opa in query.GetTable<OrderPositionAdvertisement>()
+                    .Where(x => positionIds.Contains(x.PositionId))
+                    .Where(x => !x.FirmAddressId.HasValue || firmAddressIds.Contains(x.FirmAddressId.Value))
+                from op in query.GetTable<OrderPosition>()
+                    .Where(x => x.IsActive && !x.IsDeleted)
+                    .Where(x => x.Id == opa.OrderPositionId)
+                from o in query.GetTable<Order>()
+                    .Where(x => new[] { 2, 4, 5 }.Contains(x.WorkflowStepId)) // заказы "на оформлении" не нужны - проверяемый их в любом лучае не видит
+                    .Where(x => x.IsActive && !x.IsDeleted)
+                    .Where(x => x.BeginDistributionDate < order.EndDistributionDatePlan && order.BeginDistributionDate < x.EndDistributionDatePlan && x.DestOrganizationUnitId == order.DestOrganizationUnitId)
+                    .Where(x => x.Id == op.OrderId)
+                select new { Order = o, OrderPosition = op, OrderPositionAdvertisement = opa };
+
+            store.AddRange(xxxOrders.Select(x => x.Order).Distinct().Execute());
+            store.AddRange(xxxOrders.Select(x => x.OrderPosition).Distinct().Execute());
+            store.AddRange(xxxOrders.Select(x => x.OrderPositionAdvertisement).Distinct().Execute());
+        }
+    }
+}
