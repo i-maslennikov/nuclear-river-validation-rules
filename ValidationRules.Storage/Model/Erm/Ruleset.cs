80ec31b62dca3a4048cebcf940fc33ac13871641
Масленников Иван Евгеньевич <i.maslennikov@2gis.ru>
2018-07-11 14:35:59 +0700

    model cleanup

--- ValidationRules.Storage/Model/Erm/Ruleset.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NuClear.ValidationRules.Storage.Model.Erm
-{
-    public sealed class Ruleset
-    {
-        public long Id { get; set; }
-        public int Priority { get; set; }
-        public bool IsDeleted { get; set; }
-    }
-}
