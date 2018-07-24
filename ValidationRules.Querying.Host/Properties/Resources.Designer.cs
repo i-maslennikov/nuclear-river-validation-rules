ad3086ddc2e118622daf71e4ad26311d3a9bf04f
Баранихин Юрий Владимирович <y.baranihin@2gis.ru>
2018-07-24 11:40:27 +0700

    [ERM-11728] test

--- ValidationRules.Querying.Host/Properties/Resources.Designer.cs
+++ ValidationRules.Querying.Host/Properties/Resources.Designer.cs
@@ -736,7 +736,7 @@ namespace NuClear.ValidationRules.Querying.Host.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Адрес {0} принадлежит фирме-рекламодателю {1} с заказом {2}.
+        ///   Looks up a localized string similar to Адрес {0} принадлежит фирме-рекламодателю {1} с заказом, содержащим пакет &quot;Базовый&quot; или контекстный банер {2}.
         /// </summary>
         internal static string PartnerAdvertisementCouldNotCauseProblemsToTheAdvertiser {
             get {
