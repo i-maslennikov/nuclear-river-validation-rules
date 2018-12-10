c4bc6f044996ac4f8cfc77d74df123f64b06645c
Баранихин Юрий Владимирович <y.baranihin@2gis.ru>
2018-12-10 12:54:05 +0700

    Revert "[ERM-11951] проверка PoiAmountForEntranceShouldMeetMaximumRestriction… (#349) (#350)"
    This reverts commit e5ff467405e62007e3137d5308f756d14873e1be.

--- ValidationRules.Querying.Host/CheckModes/CheckModeRegistry.cs
+++ ValidationRules.Querying.Host/CheckModes/CheckModeRegistry.cs
@@ -319,10 +319,10 @@ namespace NuClear.ValidationRules.Querying.Host.CheckModes
 
                     Rule(MessageTypeCode.PoiAmountForEntranceShouldMeetMaximumRestrictions,
                          single: RuleSeverityLevel.Warning,
-                         singleForApprove: RuleSeverityLevel.Error,
-                         manualReport: RuleSeverityLevel.Error,
-                         prerelease: RuleSeverityLevel.Error,
-                         release: RuleSeverityLevel.Error),
+                         singleForApprove: RuleSeverityLevel.Warning,
+                         manualReport: RuleSeverityLevel.Warning,
+                         prerelease: RuleSeverityLevel.Warning,
+                         release: RuleSeverityLevel.Warning),
 
                     Rule(MessageTypeCode.AtLeastOneLinkedPartnerFirmAddressShouldBeValid,
                          single: RuleSeverityLevel.Error,
