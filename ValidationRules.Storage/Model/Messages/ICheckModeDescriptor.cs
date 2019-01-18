68d716ff91f93445037c85e8443da35c640f0477
s.komlach <s.komlach@2gis.ru>
2019-01-18 19:26:59 +0700

    CR: specially for YuryBaranikhin

--- ValidationRules.Storage/Model/Messages/ICheckModeDescriptor.cs
+++ ValidationRules.Storage/Model/Messages/ICheckModeDescriptor.cs
@@ -7,12 +7,12 @@ namespace NuClear.ValidationRules.Storage.Model.Messages
 {
     public interface ICheckModeDescriptor
     {
+        CheckMode CheckMode { get; }
+
         IReadOnlyDictionary<MessageTypeCode, RuleSeverityLevel> Rules { get; }
 
         // Костыль для проверки заказа "на расторжении" только в той его части, которая ещё не размещалась
         // https://github.com/2gis/nuclear-river-validation-rules/issues/193
         DateTime GetValidationPeriodStart(Order order);
-
-        CheckMode CheckMode { get; }
     }
 }
